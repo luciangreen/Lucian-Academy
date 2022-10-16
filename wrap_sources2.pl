a47ba76bcb4585d21fa8b2930903b4f800a02898
Lucian Green <luciangreen@lucianacademy.com>
2022-10-16 16:51:07 +1100

    Added, Moved books, changed wrap_sources2

--- wrap_sources2.pl
+++ wrap_sources2.pl
@@ -1,17 +1,21 @@
 :-include('../listprologinterpreter/la_strings.pl').
 :-include('../listprologinterpreter/la_strings_string.pl').
-
 wrap_sources:-
 	directory_files("sources/",F),
 	delete_invisibles_etc(F,G),
 %%trace,
 %SepandPad="#@~%`$?-+*^,()|.:;=_/[]<>{}\n\r\s\t\\!'0123456789",
-	findall(_,(member(Filex1,G),
-	string_concat("sources/",Filex1,Filex),
-	string_concat("sources1/",Filex1,Filexx),
+	findall(_,(member(Folderx1,G),
+	string_concat("sources/",Folderx1,Folderx),
+	directory_files(Folderx,F1),
+	delete_invisibles_etc(F1,G1),
+	
+	findall(_,(member(Filex1,G1),
+	%trace,
+	foldr(string_concat,["sources/",Folderx1,"/",Filex1],Filex),
+	foldr(string_concat,["sources1/",Folderx1,"/",Filex1],Filexx),
 	
 	split_string(Filex1," "," ",Filex2),
-	%append(_,["Green"|Filex3],Filex2),
 	Filex3=Filex2,
 	findall([Filex31," "],(member(Filex31,Filex3)),Filex32),
 	maplist(append,[Filex32],[Filex321]),
@@ -20,13 +24,22 @@ wrap_sources:-
 		phrase_from_file_s(string(String00a), Filex),
 		string_codes(String02b,String00a),
 		
-		concat_list(["[\"Green, L 2021, <i>",Filex4,"</i>, Lucian Academy Press, Melbourne.\",\"Green, L 2021\",1,\"",String02b,"\"]"],Line),
-		%atom_to_term(String02b,String02a,[]),
+	string_concat(A,_,Filex1),string_length(A,4),
+	(A="dot-" ->
+	
+		Line=String02b;
 		
+	(
+		concat_list(["[\"Green, L 2021, <i>",Filex4,"</i>, Lucian Academy Press, Melbourne.\",\"Green, L 2021\",1,\"",String02b,"\"]"],Line)
+	)),
+		%atom_to_term(String02b,String02a,[]),
+
+	foldr(string_concat,["sources1/",Folderx1,"/"],Folderxx1),
+	(exists_directory(Folderxx1)->true;make_directory(Folderxx1)),		
 	(open_s(Filexx,write,Stream1),
 %%	string_codes(BrDict3),
 	write(Stream1,Line),
-	close(Stream1))),_).
+	close(Stream1))),_)),_).
 		
 delete_invisibles_etc(F,G) :-
 	findall(J,(member(H,F),atom_string(H,J),not(J="."),not(J=".."),not(string_concat(".",_,J))),G).
