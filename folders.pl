44b2b671273fa25cc7643f6357cf49f612093291
Lucian Green <luciangreen@lucianacademy.com>
2023-09-02 22:28:53 +1000

    Grammar checked Immortality 26, Medicine of Meditation 1 and Medicine of Pedagogy 1

---
+++ folders.pl
@@ -0,0 +1,1 @@
+folders(["Computational English","Creating and Helping Pedagogues","Fundamentals of Meditation and Meditation Indicators","Fundamentals of Pedagogy and Pedagogy Indicators","Medicine","Lecturer","Short Arguments","Mind Reading","Time Travel","Delegate workloads, Lecturer, Recordings"]).
