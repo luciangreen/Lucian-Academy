a14574d50b099315986868effc0a3e1c22481060
Lucian Green <luciangreen@lucianacademy.com>
2024-02-20 18:34:31 +1100

    Fixed dot, backslash and and quotes

--- wrap_sources2.pl
+++ wrap_sources2.pl
@@ -26,21 +26,27 @@ wrap_sources:-
 
 	atomic_list_concat(B,"\"",String02c),
 	atomic_list_concat(B,"\\""",C),
+	%atomic_list_concat(C1,"\\",C),
+	%atomic_list_concat(C1,"\\\\",C2),
 	atom_string(C,String02b),
 	
-	string_concat(A,_,Filex1),string_length(A,4),
-	(A="dot-" ->
+	once((string_concat(A,B1,Filex1),string_length(A,4))),
+	(not(A="dot-") ->
 	
-		Line=String02b;
+		(%trace,
+		Line=String02c,	Filexx=Filexx2)
+;
 		
-	(
-		concat_list(["[\"Green, L 2022, <i>",Filex4,"</i>, Lucian Academy Press, Melbourne.\",\"Green, L 2022\",1,\"",String02b,"\"]"],Line)
+	(	%trace,
+	foldr(string_concat,["sources1/",Folderx1,"/",B1],Filexx2),
+string_concat(B2,".txt",B1),
+		concat_list(["[\"Green, L 2024, <i>",B2,"</i>, Lucian Academy Press, Melbourne.\",","\"Green, L 2024\",",1,",\"",String02b,"\"]"],Line)
 	)),
 		%atom_to_term(String02b,String02a,[]),
-
+%term_to_atom(Line,Line1),
 	foldr(string_concat,["sources1/",Folderx1,"/"],Folderxx1),
 	(exists_directory(Folderxx1)->true;make_directory(Folderxx1)),		
-	(open_s(Filexx,write,Stream1),
+	(open_s(Filexx2,write,Stream1),
 %%	string_codes(BrDict3),
 	write(Stream1,Line),
 	close(Stream1))),_)),_).
