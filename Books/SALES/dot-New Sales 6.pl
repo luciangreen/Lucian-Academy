5e4f84a3aeee49aca60382a5360f0d3f597e927d
Lucian Green <luciangreen@lucianacademy.com>
2024-06-01 11:34:36 +1000

    Converted several philosophy files

---
+++ Books/SALES/dot-New Sales 6.pl
@@ -0,0 +1,355 @@
+New Sales 6
+
+1. The salesperson prepared for joining, expansion, enlightenment, and constructive evaluation. The novel's algorithmic perspectives were ready for critical analysis. It was as if an algorithm processing a linear structure could be simplified from a tree. This process started with input, files, API calls, or protocols. I specialised in simplifying the complex parts first. 
+2. The salesperson used attention-making methods to think of breasonings, such as GL, BAG and combophil. I found philosophies to write about using syntheses of ten algorithms. Each chapter had one set of ten parts. I explored different configurations of the philosophy, each with different computational implications. They were aphors or short paragraphs, each exploring a specific idea associated with objects in accreditation when individually thought of.
+3. The salesperson added to or nourished undeveloped, unfinished or cutting-edge frontiers for research. I connected philosophies to others in philosophy for ideas. I decided on a trajectory or contention between two ideas and explored them. These ideas were selected from idea centres in philosophy, found using key term clusters. I explored connections via other ideas between the ideas.
+4. The salesperson said their version was pitch-blackness with razor-new technology in the jewel chip. I explored hypotheses and questions and how various people could approach these over time. I thought in terms of macro-ideas, or the main ideas of the philosopher, and how these ideas were relevant. These ideas were the key terms with the highest frequencies in the texts. I retrieved a non-working algorithm by repairing its skills from scratch and approaching its original skills as closely as possible without sacrificing correctness.
+5. The salesperson chose positive stories, had characters with ardour and had a science research edge. I wrote a story as an implicit framework for philosophy. I cleaned and focused on philosophically interpreted instruments in settings. I focused on positivity, algorithms, and multiplicity. Before setting off, I checked that my pack had an algorithm, a philosophy, art, and a song.
+6. The salesperson thought of unique ideas and perfected them before thinking of them. The philosophy matched my style of creativity but was useful and suggestive of new ideas but not exhaustive. I erred on the side of caution with finishing off algorithms to exhaust them but made up new frontiers, such as selling, educating, or growing. I used music and art as inspirations, thinking into objects and creating models and simulations of my work. I devised media where there were none, filled gaps in fonts and aimed to be well-known.
+7. The salesperson asked if they were worried about myxomatosis. I included the story point as a philosophy at the start of the scene. The reason the types and the algorithm were a proof was because they defined the alphabet of symbols used in the algorithm. I checked the algorithm proved what I wanted it to by checking the data led to the correct result with correct reasoning. I felt like a rabbit rather than a lion or monkey and couldn't eat them.
+8. The salesperson considered the menu and paid with money earned using their intellect. I thought of an idea for a setting, a large jewel computer chip. These crystals refracted lasers and used quantum components faster than the speed of light. Each component could be spiritually programmed with 16,000 breasonings and accessed as if they were real. They were used for mind reading, breasoning and immortality.
+9. The salesperson encouraged people to learn meditation to become immortal and broach life's questions. I recorded how a child thought, not wrote. I considered being an adult inside a child's body as if I had frozen my age as a child. I sided with examination, longer, finished texts, and specialised in computational spiritual sciences and other algorithms. I introduced the idea of immortality and locking one's age, raising important ethical questions.
+10. The salesperson endeavoured to create art each day. I determined the story's setting, year, and properties myself. I thought into filmable sets for special effects, including artificial intelligence, such as walking on a cubic planet or following a particle through a 3D maze. I wrote a story, particularly the description of the software box, as if it was about software for a solution to the problem. This combined realism, computation perfection and the latest science.
+11. The salesperson counted and kept their weekly predicate count to their standard. I wrote a novel using algorithmic language, giving away future computer science. For example, a robot experienced aphoria or a protective connection with humans. This was due to humans writing in philosophical aphors about objects are increased with reasoning. This increase was done automatically in the future, and it was increased that it was good.
+12. * I wrote a novel about the character John Blair, who journeyed to the earth's centre like a chaotic fractal being expanded. I regularly revisited the story with new interpretations, such as how Cultural Translation Tool 3 and other algorithms' bulk processing tasks created an automation utopia, given it was carried out well. For example, banks bulk processed transactions, people received a university stipend, and employment and tax forms were automatically processed, creating a utopia of bulk automation with the need for people to be encouraged to interact by actively learning, consuming, and spiritually growing.
+
+TM
+- As or Bs to implied representation during the day
+- could be mr, with specific alg said to them
+
+songs
+- hand-composed or mc
+- 10 hand br with rest 16k
+- ask about rhythms for each part and section
+
+movies
+- mr 1-150 hand br as 10s with developed argument summary influencing lights, make-up, etc. x funnel 1 br
+- creative choices from combophil influencing each point of writing a script
+- influence setting, character, story
+- cut boring bits, ask 3 levels of why about characters' overall motivation, synthesise their top-level motivations with other chars, make into the story
+- how story is like music, mr songs based on mood of part of the overall song or use the overall song
+- cinematography - find scenes from songs
+- write story based on scenes, appropriate for mood
+- pep up motivations with exciting or br
+
+Accred, uni
+Match n hand br
+
+Also Give to objs
+
+get into songs and roles by approximating them
+- 250 br expanded to 16k
+- write song br over 3 weeks
+
+before holidays, fiverr remixes
+
+
+john Blair
+- leads to a robot utopia - people loved for saying br
+- food, overpopulation with immortality
+
+etienne
+- translation everywhere
+- children more sensitive to different frequencies because of mother speaking in different language
+- bilanguage speakers have mental problems as children because not knowing a word or aware of others not knowing a word leads to feeling of exclusion - check
+- back translation as way of learning a language has difficulties because of systematic errors, ai may make errors in checking orig and bt have same meaning
+
+galactic empires
+- human vs computer in writing br (humans should not let ai let them become lazy in writing br), paraphrasing (humans should paraphrase by completing changing grammar, think of different synonyms and object to words with wrong meanings, use pedagogy essay structure to find sentences to connect to have structure in their essay)
+- space travel needs the sim like overpop, imm - lucian thinks of how to achieve this
+
+- aligning notes
+- pads as end to end
+- drums own rhythm
+- solo instruments unique rhythm in section
+- some parts have own rhythm
+
+song 250 hand br
+
+Programming language
+To change certain rows and columns in music comp rhythms, melody and harmony, lyrics 
+Do voice recording 
+With Sheet music 
+
+Rhythm from first line, curtail rest of tas, make titis if no notes left
+
+Different music everywhere in composition
+
+
+16k ai for psych medic
+
+Lyrics2length is n notes in rhythm
+
+Use rhythm of longest not first line
+
+Different rhythms by parts
+
+Rhythm can be entered as ta titi
+
+Optionally With note n ta1
+
+Mixture of ta ta1
+
+No 1s
+Ta1 v
+
+strings to grammar
+Do single list bbs first, abab etc 
+Then do dec tree
+
+la
+Buy product at price 
+
+update paraph lp
+
+Foldr string concat a b c
+
+Medic 16krole
+- also song 16k
+
+save same name x saves with curr date to avoid overwriting
+
+save rhythm v
+
+1 rhythm for whole song
+converts song to a rhythm
+saves rhythm in meta file, where algs can open both types of files x save rhythm separately
+rests, check rhythm length x no starting times >= 4 and covers all 4 notes
+can indicate note of first pitch, note of 2nd etc.
+
+vel
+
+[time,type,length,nth_note_from_mel_harm,velocity]
+[[0,n,1/2,1,80],
+...
+]
+
+ui composer first
+
+later: make songs based on length of syllables, fills in rest of bar with quavers to fit them in
+
+
+
+song gmail notes
+
+rhythm in mus comp
+- 3 (vocal, mel, harm) x each section of each part
+- pretty prints rhythm for editing
+- in ci cd song
+
+write queue to prevent la ws data loss
+- queue adder runs every 5 minutes
+
+sales people should be developed from birth
+- 1 for birth, 5 for h1s
+
+names in la ws
+
+Increase gh la "development" commit to 16k, 16k det
+- repos too
+x use curr texts, claim that they are (increased from) them
+
+2*16k for maharishi
+have centres x already teach medit courses
+
+accredit agency 16k*2 + 16k*2 for la (until qual runs out)
+- replace course names with advanced diploma etc
+
+if charging the same as uni, need lecture slides, lecturers, lectures
+
+- build up to uni from small la
+
+algs=dets
+ 
+web editor multifile
+- can command click file name to rename, find it in finder
+
+
+la tax
+
+double-check whether the employee has logged in
+to be paid
+
+hire an employee who can
+- hire teachers
+ - check their tfn declarations
+ - (I change their password if they are terminated)
+ - employee contract
+ - check their super forms
+ - I pay them
+ 
+ need
+ - to change pw
+ - to set up an employee 
+  - check off their 2 forms are complete
+ - bank acct
+ - pay right tax rate and amount
+ - I will mr active employees (and whether they have done work) and pay using these
+ - mr software - do before next time
+  - give As for each part (I have a practicum form that checks off tasks and lets me know their progress) - I can mr this form
+  
+  - hire a spiritual teacher myself, then make process spiritual for bus employee
+  
+  - automatically regularly check whether I need to do employees' jobs for them
+  - mr website choices, 16k for right choice - have a real employee to learn how to use systems x lighter x casual
+  
+  first: test spiritual money works
+  
+  
+  https://www.ato.gov.au/businesses-and-organisations/hiring-and-paying-your-workers/engaging-a-worker/before-you-hire-your-first-worker
+  
+  https://www.fairwork.gov.au/employment-conditions/awards/award-and-agreement-free-wages-and-conditions
+  
+  https://www.fairwork.gov.au/pay-and-wages/minimum-wages
+
+bigger people db
+Hire bus mans, teachers, teacher leaders
+- check min wage
+- check award laws
+- dismissal, policy
+- man hires, writes fin reports, enables more activity
+Ws
+- multiple centres (classes of students)
+- longer degrees 
+-accred x
+-vps for multiple As, quantum As for (only find essay, up to 80 br, rest quantum) whole thing is spiritual
+- 80 br for accred for everyone per day
+-fin reports 
+Trans ws with data files
+Main langs
+
+Grievance, feedback, credit refund
+
+Close centre
+Ads
+Fb
+do skills for my cs work
+to complete algs more quickly
+move repos to contabo
+la
+
+don't automatically write or assess if enough students/teachers
+
+mans have to interview people on zoom
+system has to list positions to hire for
+if no one then system marks
+cap numbers as a result
+
+paraph with optional x always on x optional x always on (proofreading) synonym
+proofread this text with ai
+
+also for tutor emails with explanations
+ask ai if it understands q
+- limited length and number of qs
+- no algs
+
+bot, human tutors uses message system
+
+enrolment system - introduce when have teacher and manager
+
+go back to ssi and test ws
+- back button rather than backlinks tested
+
+stop bot checkbox
+
+office for uni
+
+fun (games) br about thought
+accredited updated every hour - 250 x 16k file br pointing to 5-16k day's work - if checked
+
+
+27 5 24
+
+ssi can run ws's
+- return to a link to the main server at end
+
+doctor's appointment
+
+try sale in district
+
+sp employee
+
+names
+
+improve ass
+
+mr for sys - see if can make conns otherwise use chatgpt (pay x) - for in-person, anyway (problematise some thoughts, ass's)
+- 1-3 br per st per thought at at each point
+3 because bus, teacher join in for rest of time
+- after degree - random br and gl br per thought
+- 5 per day from same dept, linked (3-funnel with 10 gl), alg (skills for alg with 1,2,3 or n symbolising scaled, read ds, with command type + det) link to 16k thought, done at start 
+
+* mr give repres 16k to listen * cap pun way of thinking to turn down volume
+- separate bus man lessens work * but I pay them x check enough money in acct
+- listen to them individ, do work with 16*16k
+ - detect important x all repres with funnel (containing, not negating key term)
+ - refund/credit, leaving (don't listen), enrolling
+ - (not done) on phil: agree/dis with each word, rel word
+ - mr with sp systems
+ - rand mr to save time
+ 
+** flowchart of alg skills
+** 32k fast at a time (sh)
+
+showbiz dept 16ks per thought, including hiring once
+
+need song 16ks (long 16k+showbiz per idea line with2-3 ai br)
+
+put site in public
+
+cap st num x see if need new teachers because of num studs
+
+edit br num
+
+chatgpt daily sent counter - sometimes don't need more
+- keep tt As
+- keep ped ce for roles
+- la ws - one 16k, 16k using t2b file for rest
+
+- roles are 16k + 7*file
+- sometimes x 5 not 16 v
+
+manually pay employees
+
+later: courses, classes
+
+say where to download ebooks, music, algs
+alg, books, music
+just la ws 3
+- add receipt, price, product
+gst 10%
+
+
+movies
+- documentary of algorithms
+- fly-through data structures
+- music based on mus comp
+- wear suit
+- set in oxford office
+
+- buy software to make films
+
+As for songs and videos - 16k
+- with file 16ks for film, sound objects
+
+- each song has own video explaining algorithm
+ - insert text
+ 
+make movies to earn roles
+other products
+department as
+catch up today
+put in la ws3
+ebooks comp eng, music, algs
+
+music args in music book
+prof dev with vps
+find a hollywood seen as version and song for each role
+
+Prolog can compare different variables
+
+Macos
+
+new window for current window
+
+- don't deselect files in finder if click on one
