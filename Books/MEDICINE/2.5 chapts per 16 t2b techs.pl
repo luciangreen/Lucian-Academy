f2c2c8686ec3b70a63b4e927d64f258982a977b6
Lucian Green <greenlucian1@gmail.com>
2026-02-02 16:34:20 +1100

    Help Earn High Distinctions 1

--- Books/MEDICINE/2.5 chapts per 16 t2b techs.pl
+++ Books/MEDICINE/2.5 chapts per 16 t2b techs.pl
@@ -1,1 +1,21 @@
 1000/(80*5)=2.5 chapts per 16 t2b techs
+
+help earn high distinctions
+support but not conceive healthy children
+earn jobs
+sell products
+produce quantum energy
+produce quantum gravity
+project simulations
+read objects
+create quantum computers
+prevent headaches
+perform spiritual surgery
+replicate 
+vaporise items such as spiritual food or supplements
+read minds
+display spiritual screens
+time travel
+become immortal
+
+note 17 not 16 t2b techs
